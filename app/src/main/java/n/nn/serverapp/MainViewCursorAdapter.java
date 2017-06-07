24dc07d11b66542860574b28dff42fcebe7cd42f
Nithinpaul <nithpl@gmail.com>
2017-06-07 18:09:10 +0530

    Initial commit

---
+++ app/src/main/java/n/nn/serverapp/MainViewCursorAdapter.java
@@ -0,0 +1,35 @@
+package n.nn.serverapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+public class MainViewCursorAdapter extends CursorAdapter {
+    public MainViewCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+
+        return LayoutInflater.from(context).inflate(
+                R.layout.main_list_item,viewGroup,false );
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        String contactName = cursor.getString(
+                cursor.getColumnIndex(DBOpenHelper.CONTACT_NAME));
+        String contactPhone = cursor.getString(
+                cursor.getColumnIndex(DBOpenHelper.CONTACT_PHONE));
+        TextView nameTextView = (TextView) view.findViewById(R.id.nameTextView);
+        TextView phoneTextView = (TextView) view.findViewById(R.id.phoneTextView);
+        nameTextView.setText(contactName);
+        phoneTextView.setText(contactPhone);
+
+    }
+}
